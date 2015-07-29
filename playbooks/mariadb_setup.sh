55733d46656810d17a4a94b3c1feec0f582af742
Charon-Shi <chaos.scl@gmail.com>
2015-07-29 18:37:06 -0400

    Update mariadb_setup.sh

--- playbooks/mariadb_setup.sh
+++ playbooks/mariadb_setup.sh
@@ -5,14 +5,17 @@ IPA=$1
 REALM=$2
 DB=$3
 PASSWD=$4
-DB_USER=keystone
+DB_USER=test_kerberos
 IPA_USER=keystone
 LOCAL_IP=`hostname -I`
 LOCAL_IP="${LOCAL_IP%"${LOCAL_IP##*[![:space:]]}"}"
+#_STR=`getent passwd $IPA_USER`
+#_EUID=`echo $_STR |  cut -d ':' -f 3`
+_EUID=163
 
 sync_ntp $IPA
-remove_mariadb
+#remove_mariadb
 kinit_admin $PASSWD
 install_mariadb $IPA $REALM $DB $DB_USER $IPA_USER
-get_keytab $PASSWD $REALM $IPA $IPA_USER
+get_keytab $PASSWD $REALM $IPA $IPA_USER $_EUID $DB_USER
 create_keystonerc $PASSWD $LOCAL_IP
