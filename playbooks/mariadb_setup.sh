1bbf510e1c2ddf4af3efc26c2c9afd6236c90c8a
Charon-Shi <chaos.scl@gmail.com>
2015-08-05 14:10:55 -0400

    Update mariadb_setup.sh

--- playbooks/mariadb_setup.sh
+++ playbooks/mariadb_setup.sh
@@ -5,17 +5,47 @@ IPA=$1
 REALM=$2
 DB=$3
 PASSWD=$4
-DB_USER=test_kerberos
-IPA_USER=keystone
+
+KEYSTONE_DB_USER=test_keystone
+NOVA_DB_USER=test_nova
+GLANCE_DB_USER=test_glance
+CINDER_DB_USER=test_cinder
+
+KEYSTONE_IPA_USER=keystone
+NOVA_IPA_USER=nova
+GLANCE_IPA_USER=glance
+CINDER_IPA_USER=cinder
+
+KEYSTONE_DB_NAME=keystone
+NOVA_DB_NAME=nova
+GLANCE_DB_NAME=glance
+CINDER_DB_NAME=cinder
+
+KEYSTONE_EUID=163
+NOVA_EUID=162
+GLANCE_EUID=161
+CINDER_EUID=165
+
 LOCAL_IP=`hostname -I`
 LOCAL_IP="${LOCAL_IP%"${LOCAL_IP##*[![:space:]]}"}"
-#_STR=`getent passwd $IPA_USER`
-#_EUID=`echo $_STR |  cut -d ':' -f 3`
-_EUID=163
 
 sync_ntp $IPA
+# no need to remove mariadb if using mariadb-galera
 #remove_mariadb
 kinit_admin $PASSWD
-install_mariadb $IPA $REALM $DB $DB_USER $IPA_USER
-get_keytab $PASSWD $REALM $IPA $IPA_USER $_EUID $DB_USER
-create_keystonerc $PASSWD $LOCAL_IP
+install_mariadb $IPA $REALM $DB
+
+create_db_user $KEYSTONE_DB_USER $KEYSTONE_IPA_USER $REALM $KEYSTONE_DB_NAME
+get_keystone_keytab $PASSWD $REALM $IPA $KEYSTONE_IPA_USER $KEYSTONE_EUID $KEYSTONE_DB_USER
+
+create_db_user $NOVA_DB_USER $NOVA_IPA_USER $REALM $NOVA_DB_NAME
+get_nova_keytab $PASSWD $REALM $IPA $NOVA_IPA_USER $NOVA_EUID $NOVA_DB_USER
+
+create_db_user $GLANCE_DB_USER $GLANCE_IPA_USER $REALM $GLANCE_DB_NAME
+get_glance_keytab $PASSWD $REALM $IPA $GLANCE_IPA_USER $GLANCE_EUID $GLANCE_DB_USER
+
+create_db_user $CINDER_DB_USER $CINDER_IPA_USER $REALM $CINDER_DB_NAME
+get_cinder_keytab $PASSWD $REALM $IPA $CINDER_IPA_USER $CINDER_EUID $CINDER_DB_USER
+
+#TODO use the keystonerc_admin instead for now
+#create_keystonerc $PASSWD $LOCAL_IP
