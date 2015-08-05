3c27f2f06b9445690a868ec62ccab2b450ed639a
Charon-Shi <chaos.scl@gmail.com>
2015-08-05 14:05:04 -0400

    Update ipa_setup.sh

--- playbooks/ipa_setup.sh
+++ playbooks/ipa_setup.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 PASSWD=$1
+KEYSTONE_IPA_USER=keystone
+NOVA_IPA_USER=nova
+GLANCE_IPA_USER=glance
+CINDER_IPA_USER=cinder
 
 echo $PASSWD | kinit admin
 ipa config-mod --emaildomain=
-printf "keystone\n$PASSWD\n$PASSWD" | ipa user-add --last=Administrator --first=none --uid=163 --gidnumber=163 --password
-printf "$PASSWD\n$PASSWD\n$PASSWD" | kinit keystone
+
+printf "$KEYSTONE_IPA_USER\n$PASSWD\n$PASSWD" | ipa user-add --last=Administrator --first=none --uid=163 --gidnumber=163 --password
+printf "$PASSWD\n$PASSWD\n$PASSWD" | kinit $KEYSTONE_IPA_USER
+
+echo $PASSWD | kinit admin
+printf "$NOVA_IPA_USER\n$PASSWD\n$PASSWD" | ipa user-add --last=Administrator --first=none --uid=162 --gidnumber=162 --password
+printf "$PASSWD\n$PASSWD\n$PASSWD" | kinit $NOVA_IPA_USER
+
+echo $PASSWD | kinit admin
+printf "$GLANCE_IPA_USER\n$PASSWD\n$PASSWD" | ipa user-add --last=Administrator --first=none --uid=161 --gidnumber=161 --password
+printf "$PASSWD\n$PASSWD\n$PASSWD" | kinit $GLANCE_IPA_USER
+
+echo $PASSWD | kinit admin
+printf "$CINDER_IPA_USER\n$PASSWD\n$PASSWD" | ipa user-add --last=Administrator --first=none --uid=165 --gidnumber=165 --password
+printf "$PASSWD\n$PASSWD\n$PASSWD" | kinit $CINDER_IPA_USER
